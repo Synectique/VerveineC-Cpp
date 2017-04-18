25928911c1f480c79e6318058fdccdad65e747d0
anquetil <nicolas.anquetil@inria.fr>
2017-04-18 17:00:22 +0200

    forgot the WrongClassGuess exception

---
+++ src/eu/synectique/verveine/extractor/utils/WrongClassGuessException.java
@@ -0,0 +1,28 @@
+package eu.synectique.verveine.extractor.utils;
+
+import eu.synectique.verveine.core.gen.famix.NamedEntity;
+
+/**
+ * An exception to flag entities wrongly typed during parsing (e.g. a Namespace that was expected as a Class.
+ * It often happens because the entity was first created without really knowing what it is.
+ * 
+ * Class also defines a static method to log such errors 
+ */
+public class WrongClassGuessException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public WrongClassGuessException(String message) {
+		super(message);
+	}
+
+	static public <T extends NamedEntity> void reportWrongClassGuess(Class<T> clazz, NamedEntity found) {
+		System.err.print(("Exception: wrong guessed type '"+found.getClass().getSimpleName()+"' instead of expected type '"+clazz.getSimpleName() + "' for entity "+found.getName()));
+		if (found.getSourceAnchor() != null) {
+			System.err.println(" @" + found.getSourceAnchor());
+		}
+		else {
+			System.err.println();
+		}
+	}
+ 
+}
